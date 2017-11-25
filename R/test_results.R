fe96814753a924441512dee0aa3f75e842f46b06
Dominik Vogel <dominik.vogel.ii@uni-potsdam.de>
2017-11-25 18:20:44 +0100

    update cho_results

--- R/test_results.R
+++ R/test_results.R
@@ -1,13 +1,15 @@
+# Chi2
 chi_results <- function(x, y) {
   devtools::use_package("stats")
   devtools::use_package("broman")
   suppressWarnings(chi_result <- chisq.test(table(x, y)))
+  p <- ifelse(chi_result[["p.value"]] >= 0.001,
+              paste0("p = ", broman::myround(chi_result[["p.value"]], 2)),
+              "p < 0.001")
   result <- paste0("Chi2(",
-         as.integer(chi_result[["parameter"]][["df"]]),
-         ") = ",
-         broman::myround(chi_result[["statistic"]][["X-squared"]], 2),
-         "; p = ",
-         broman::myround(chi_result[["p.value"]], 2))
+         as.integer(chi_result[["parameter"]][["df"]]), ") = ",
+         broman::myround(chi_result[["statistic"]][["X-squared"]],2), "; ",
+         p)
   return(result)
 }
 
