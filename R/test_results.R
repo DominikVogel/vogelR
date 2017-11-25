a890d66eb2811665e3dbbb930a5f028b1b4f6bbd
Dominik Vogel <dominik.vogel.ii@uni-potsdam.de>
2017-11-25 18:20:59 +0100

    add t_results()

--- R/test_results.R
+++ R/test_results.R
@@ -15,3 +15,33 @@ chi_results <- function(x, y) {
 
 
 
+
+# t-test
+t_results <- function(x, y, alternative = "two.sided",
+                      mu = 0, conf.level = 0.95,
+                      paired = FALSE, var.equal = FALSE) {
+  devtools::use_package("stats")
+  devtools::use_package("broman")
+  t_result <- t.test(y ~ x,
+                     alternative = alternative,
+                     mu = mu, paired = paired,
+                     conf.level = conf.level,
+                     var.equal = var.equal)
+  p <- ifelse(t_result[["p.value"]] >= 0.001,
+              paste0("p = ", broman::myround(t_result[["p.value"]], 2)),
+              "p < 0.001")
+  result <- paste0("t(",
+                   broman::myround(t_result[["parameter"]][["df"]], 2),
+                   ") = ",
+                   broman::myround(t_result[["statistic"]][["t"]], 2), "; ",
+                   p)
+  return(result)
+}
+
+
+set.seed(1)
+df <- data.frame(happy = c(rnorm(n = 10, mean = 3, sd = 0.5), rnorm(n = 10, mean = 4, sd = 0.5)),
+                 treatment = c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                               1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
+
+t_results(y = df$happy, x = df$treatment)
